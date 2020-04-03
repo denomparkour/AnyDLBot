f7321dc3bba47cfba3bc3911cb0fae0c8188dfa7
Denomparkour <42404684+denomparkour@users.noreply.github.com>
2020-04-03 18:05:36 +0530

    Update unzip.py

--- plugins/unzip.py
+++ plugins/unzip.py
@@ -23,7 +23,7 @@ else:
 from translation import Translation
 
 import pyrogram
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
